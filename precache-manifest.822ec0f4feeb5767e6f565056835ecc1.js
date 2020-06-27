106b7be3da91a28a038c5ac9eccfc27d3f2a4c38
Abdul Ahad <ahad.jnv22@gmail.com>
2020-06-27 13:16:22 +0530

    Updates

---
+++ precache-manifest.822ec0f4feeb5767e6f565056835ecc1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a0d7be34f89e1a2cbaf1aaab25f687",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "b1f578167a2dc760fb83",
+    "url": "/personal-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cf0937959f1ebe3acff2",
+    "url": "/personal-portfolio/static/css/main.fa8cf8f5.chunk.css"
+  },
+  {
+    "revision": "b1f578167a2dc760fb83",
+    "url": "/personal-portfolio/static/js/2.4e500f7b.chunk.js"
+  },
+  {
+    "revision": "b894f929bf31890c9b3f1eccedd42faf",
+    "url": "/personal-portfolio/static/js/2.4e500f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0937959f1ebe3acff2",
+    "url": "/personal-portfolio/static/js/main.46f45adf.chunk.js"
+  },
+  {
+    "revision": "1fd8320ed472344e7a3b",
+    "url": "/personal-portfolio/static/js/runtime-main.41dc66a3.js"
+  },
+  {
+    "revision": "0695adaf570eb4535b4e6f2b0f06e02a",
+    "url": "/personal-portfolio/static/media/about_pic.0695adaf.jpg"
+  },
+  {
+    "revision": "3775a66c9a44a19ff4db398f0555aad4",
+    "url": "/personal-portfolio/static/media/circuit.3775a66c.jpg"
+  },
+  {
+    "revision": "c32b9563d92b00a6a921fe16d418ecf5",
+    "url": "/personal-portfolio/static/media/farmingbot.c32b9563.jpg"
+  },
+  {
+    "revision": "46a777a5313000e9c9576ed4bb73e637",
+    "url": "/personal-portfolio/static/media/github-brands.46a777a5.svg"
+  },
+  {
+    "revision": "988c54e007fe74d9ed38d0af473f21ee",
+    "url": "/personal-portfolio/static/media/linkedin-brands.988c54e0.svg"
+  },
+  {
+    "revision": "6f1f85f3a1c1a07ace28f724b77de4c2",
+    "url": "/personal-portfolio/static/media/moon-regular.6f1f85f3.svg"
+  },
+  {
+    "revision": "98885aeabc1661f7531f95364cd26be3",
+    "url": "/personal-portfolio/static/media/msk.98885aea.jpg"
+  },
+  {
+    "revision": "0d95fc80258e79bf8ec81c62aa4b3f3e",
+    "url": "/personal-portfolio/static/media/qr.0d95fc80.jpg"
+  },
+  {
+    "revision": "37372854aeaa60e33479bc435202df43",
+    "url": "/personal-portfolio/static/media/raman.37372854.png"
+  },
+  {
+    "revision": "8afcca4b652b04c4f4db5e5304ba0815",
+    "url": "/personal-portfolio/static/media/sharecab.8afcca4b.jpg"
+  },
+  {
+    "revision": "30846890c2904bb8d79db7dff99a73d6",
+    "url": "/personal-portfolio/static/media/stack-overflow-brands.30846890.svg"
+  },
+  {
+    "revision": "fbffcf4c0463919a493c67a071c8ec21",
+    "url": "/personal-portfolio/static/media/sun-regular.fbffcf4c.svg"
+  },
+  {
+    "revision": "532a2219a6368c6d766db050403c2c46",
+    "url": "/personal-portfolio/static/media/youtube-brands.532a2219.svg"
+  }
+]);
